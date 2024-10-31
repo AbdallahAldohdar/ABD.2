fdc1b5bff40b15516b66892a06cb13f0dd2fd4fd
AbdallahAldohdar <aboodmhammad224@gmail.com>
2024-10-31 19:37:54 +0200

    Very important update to my cod  !!!

--- Assets/Script/abood.cs
+++ Assets/Script/abood.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class abood : MonoBehaviour
@@ -17,12 +20,20 @@ public class abood : MonoBehaviour
         if (randomNumber==15) break;
         Debug.Log("Abdallah");
         }
-       
+        string[] words = { "Cat", "Dog", "Car", "Pizza", "Hat", "Fish", "Tree", "Monkey", "Ball", "Bird" };
+        string sentence = "";
+
+        int wordCount = 0;
+        
+        while (wordCount < 7)
+        {
+            int index = Random.Range(0,words.Length); // Get a random index from the array
+            sentence += words[index] + " , "; // Add the chosen word to the sentence
+            wordCount++;
+        }
+
+        Debug.Log("Funny Sentence: " + sentence.Trim());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
